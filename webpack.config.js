84a00c15e3912813081bc776b3d6ee9d0357749e
Kane <dmccer@gmail.com>
2015-11-27 10:45:00 +0800

    config webpack for react es6

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: {
+    index: './src/asset/js/index.js',
+    vendor: ['./bower_components/zepto/zepto.js']
+  },
+  output: {
+    path: path.join(__dirname, 'dist', 'js'),
+    publicPath: 'dist/js/',
+    filename: '[name].bundle.js',
+    chunkFilename: "[id].chunk.js"
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js')
+  ],
+  module: {
+    preLoaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'source-map'
+    }],
+    loaders: [{
+      test: /\.less$/,
+      loaders: [
+        'style',
+        'css',
+        'less',
+        'autoprefixer?browsers=last 3 versions'
+      ]
+    }, {
+      test: /\.css$/,
+      loaders: [
+        'style',
+        'css'
+      ]
+    }, {
+      test: /\.(png|jpg|svg|ttf)(#[a-zA-Z]*)$/,
+      loaders: [
+        'url?limit=8192',
+        'img'
+      ]
+    }, {
+      test: /\.(html|htm)$/,
+      loader: 'html-loader'
+    }, {
+      test: /\.(woff|eot)(#[a-zA-Z]*)$/,
+      loader: 'file-loader'
+    }, {
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loaders: [
+        'react-hot',
+        'babel?presets[]=react,presets[]=es2015'
+      ]
+    }]
+  }
+};
