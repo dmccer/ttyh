6dab3b16115a5136acfbfc3f68338f96a98e4eb1
Kane <dmccer@gmail.com>
2015-12-01 14:19:59 +0800

    社区根据尺寸调整

--- src/asset/js/bbs/active-user/index.js
+++ src/asset/js/bbs/active-user/index.js
@@ -11,24 +11,26 @@ export default class Post extends React.Component {
     return (
       <section className="active-user">
         <h2 className="subtitle">人气用户</h2>
-        <div className="active-users row">
-          <div className="avatar">
-            <a href="#"><img src="" /></a>
-          </div>
-          <div className="avatar">
-            <a href="#"><img src="" /></a>
-          </div>
-          <div className="avatar">
-            <a href="#"><img src="" /></a>
-          </div>
-          <div className="avatar">
-            <a href="#"><img src="" /></a>
-          </div>
-          <div className="avatar">
-            <a href="#"><img src="" /></a>
-          </div>
-          <div className="avatar">
-            <a href="#"><img src="" /></a>
+        <div className="active-users">
+          <div className="row">
+            <div className="avatar">
+              <a href="#"><img src="" /></a>
+            </div>
+            <div className="avatar">
+              <a href="#"><img src="" /></a>
+            </div>
+            <div className="avatar">
+              <a href="#"><img src="" /></a>
+            </div>
+            <div className="avatar">
+              <a href="#"><img src="" /></a>
+            </div>
+            <div className="avatar">
+              <a href="#"><img src="" /></a>
+            </div>
+            <div className="avatar">
+              <a href="#"><img src="" /></a>
+            </div>
           </div>
         </div>
       </section>
