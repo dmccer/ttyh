3474ce155fb5b84da9b618f2a7fc055a52288e07
yjy <yjy@ttyhuo.cn>
2015-12-19 17:58:44 +0800

    login refact

--- src/asset/js/login/index.js
+++ src/asset/js/login/index.js
@@ -6,6 +6,7 @@ import './index.less';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Poptip from '../poptip/';
+import Loading from '../loading/';
 import querystring from 'querystring';
 
 export default class Login extends React.Component {
@@ -13,6 +14,24 @@ export default class Login extends React.Component {
     super();
 
     this.state = {};
+  }
+
+  validateTel() {
+    const tel = $.trim(this.state.tel);
+
+    if (tel === '') {
+      this.refs.poptip.warn('请输入手机号');
+
+      return false;
+    }
+
+    if (tel.length !== 11) {
+      this.refs.poptip.warn('手机号格式不正确');
+
+      return false;
+    }
+
+    return true;
   }
 
   handleSubmit(e: Object) {
@@ -24,13 +43,24 @@ export default class Login extends React.Component {
       return;
     }
 
+    let url, data = {
+      confirmCode: this.state.verifyCode
+    };
+
+    if (this.state.loggingUserSnapShotKey) {
+      url = '/mvc/login_confirmJsonNew';
+      data.loggingUserSnapShotKey = this.state.loggingUserSnapShotKey;
+    }
+
+    if (this.state.draftUserSnapShotKey) {
+      url = '/mvc/registerJsonNew';
+      data.draftUserSnapShotKey = this.state.draftUserSnapShotKey;
+    }
+
     $.ajax({
-      url: '/mvc/login_confirmJson',
+      url: url,
       type: 'POST',
-      data: {
-        loggingUserSnapShotKey: this.state.loggingUserSnapShotKey,
-        password: this.state.password
-      },
+      data: data,
       success: (res) => {
         if (res.viewName === 'user/home') {
           this.refs.poptip.success('登录成功');
@@ -46,14 +76,48 @@ export default class Login extends React.Component {
           return;
         }
 
-        if (res.viewName === 'user/login/confirm') {
-          this.refs.poptip.success('密码不正确');
+        let msg;
+
+        switch (res.viewName) {
+          case 'user/login/confirm':
+          case 'user/register':
+            msg = '验证码不正确';
+            break;
+          case 'user/login':
+            msg = '系统异常';
+            break;
+          case 'err':
+            msg = res.errMsg;
+            break;
         }
+
+        this.refs.poptip.warn(msg);
       },
       error: () => {
         this.refs.poptip.error('系统异常');
       }
     })
+  }
+
+  handleGetVerifyCode(e: Object) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (!this.validateTel()) {
+      return;
+    }
+
+    if (this.state.verifyCodeDisabled) {
+      this.refs.poptip.info('验证码已发送');
+
+      return;
+    }
+
+    this.handleValidateTelRemote(() => {
+      this.setState({
+        verifyCodeDisabled: true
+      });
+    });
   }
 
   handleTelChange(e: Object) {
@@ -64,19 +128,40 @@ export default class Login extends React.Component {
     });
   }
 
-  handleFocusPassword(e: Object) {
+  handleValidateTelRemote(cb) {
+    this.refs.loading.show('正在获取验证码...');
+
     $.ajax({
-      url: '/mvc/loginJson',
+      url: '/mvc/loginJsonNew',
       type: 'POST',
       data: {
         accountNo: this.state.tel
       },
       success: (res) => {
+        this.refs.loading.close();
+
         if (res.viewName === 'user/login/confirm') {
+          this.refs.poptip.success('验证码发送成功');
+
           this.setState({
             tip: null,
             loggingUserSnapShotKey: res.loggingUserSnapShotKey
           });
+
+          cb();
+
+          return;
+        }
+
+        if (res.viewName === 'user/register') {
+          this.refs.poptip.success('验证码发送成功');
+
+          this.setState({
+            tip: null,
+            draftUserSnapShotKey: res.draftUserSnapShotKey
+          });
+
+          cb();
 
           return;
         }
@@ -89,9 +174,6 @@ export default class Login extends React.Component {
             break;
           case 'user/home':
             msg = '服务器已登录';
-            break;
-          case 'user/register':
-            msg = '该号码尚未注册';
             break;
         }
 
@@ -107,7 +189,23 @@ export default class Login extends React.Component {
     });
   }
 
-  handlePasswordChange(e: Object) {
+  handleNumChange(field: string, e: Object) {
+    let o = {};
+
+    o[field] = $.trim(e.target.value).replace(/[^\d]+/g, '');
+
+    this.setState(o);
+  }
+
+  handleStrChange(field: string, e: Object) {
+    let o = {};
+
+    o[field] = $.trim(e.target.value);
+
+    this.setState(o);
+  }
+
+  handleVerifyCodeChange() {
     this.setState({
       password: e.target.value
     });
@@ -126,34 +224,33 @@ export default class Login extends React.Component {
                   type="tel"
                   placeholder="输入手机号"
                   value={this.state.tel}
-                  onChange={this.handleTelChange.bind(this)}
+                  onChange={this.handleNumChange.bind(this, 'tel')}
                 />
               </div>
             </div>
             <div className="field">
-              <label><i className="icon icon-lock s20"></i></label>
+              <label><i className="icon icon-key s20"></i></label>
               <div className="control">
                 <input
-                  type="password"
-                  placeholder="输入密码"
-                  value={this.state.password}
-                  onFocus={this.handleFocusPassword.bind(this)}
-                  onChange={this.handlePasswordChange.bind(this)}
+                  type="tel"
+                  className="verify-code"
+                  placeholder="输入验证码"
+                  value={this.state.verifyCode}
+                  onChange={this.handleNumChange.bind(this, 'verifyCode')}
                 />
+                <button
+                  type="button"
+                  className="btn teal verify-tip-btn"
+                  onClick={this.handleGetVerifyCode.bind(this)}
+                  disabled={this.state.verifyCodeDisabled}>获取验证码</button>
               </div>
             </div>
           </div>
-          <p className="forget">
-            <a href="./retrieve.html">
-              <i className="icon icon-question s20"></i>
-              忘记密码
-            </a>
-          </p>
           <div className="actions">
             <button className="btn block teal" type="submit">登录</button>
-            <a href="./register.html" className="btn block teal-line">快速注册</a>
           </div>
         </form>
+        <Loading ref="loading" />
         <Poptip ref="poptip" />
       </section>
     );
