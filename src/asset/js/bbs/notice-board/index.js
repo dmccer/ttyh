97d5fe92bba15368ff6d87aa4861cee937d50c24
Kane <dmccer@gmail.com>
2015-12-21 16:29:40 +0800

    公告

--- src/asset/js/bbs/notice-board/index.js
+++ src/asset/js/bbs/notice-board/index.js
@@ -21,9 +21,35 @@ export default class NoticeBoard extends React.Component {
         this.setState({
           time: data.bbsForumList[0].create_time,
           text: data.bbsForumList[0].content
-        })
+        });
+
+        this.scroll();
       }
-    })
+    });
+  }
+
+  scroll() {
+    let $content = $(this.refs.content);
+    let $text = $(this.refs.text);
+
+    let len = $text.width();
+    let w = $content.width();
+
+    let delta = len - w;
+
+    let fn = () => {
+      $text.animate({
+        left: -delta
+      }, this.state.text.length / 3 * 1000, 'linear', () => {
+        $text.css({
+          left: 0
+        });
+
+        setTimeout(fn, 2000);
+      });
+    }
+
+    fn();
   }
 
   render() {
@@ -33,7 +59,7 @@ export default class NoticeBoard extends React.Component {
           <i className="tag purple">公告</i>
         </div>
         <div className="nb-time"><ReadableTime time={this.state.time} /></div>
-        <div className="nb-content"><p>{this.state.text}</p></div>
+        <div className="nb-content" ref="content"><p className="nb-text" ref="text">{this.state.text}</p></div>
       </section>
     );
   }
