067a2aca771f1e6e7f665176473b127b6700574c
dmccer <dmccer@gmail.com>
2016-03-30 14:19:16 +0800

    常量定义

---
+++ src/asset/js/const/fetch.js
@@ -0,0 +1,22 @@
+import assign from 'lodash/object/assign';
+
+const COMMON_OPT = {
+  credentials: 'same-origin'
+}
+const COMMON_HEADER = {
+  'Accept': 'application/json',
+}
+const FORM_POST_HEADER = {
+  'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+};
+
+
+export const POST_OPT = assign({}, COMMON_OPT, {
+  method: 'POST',
+  headers: assign({}, COMMON_HEADER, FORM_POST_HEADER)
+});
+
+export const GET_OPT = assign({}, COMMON_OPT, {
+  method: 'GET',
+  headers: COMMON_HEADER
+});
---
+++ src/asset/js/const/wx.js
@@ -0,0 +1,4 @@
+export default {
+  wxAppId: 'wx13306fcc7460426e',
+  wxSignatureUrl: '/api/bbs_v2/jsapi'
+}
