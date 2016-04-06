6c85dc052864495e153736538507c59479213cd3
dmccer <dmccer@gmail.com>
2016-04-06 18:55:15 +0800

    tag cell-from

---
+++ src/asset/js/const/truck-pub.js
@@ -0,0 +1,7 @@
+// 添加车辆草稿
+export const TRUCK_ADD_DRAFT = 'truck_add_draft';
+// 发车页面用户
+export const PAGE_TYPE = 'trucker_page';
+
+// 选择的常跑路线
+export const SELECTED_COMMON_ROUTE = 'selected_common_route';
