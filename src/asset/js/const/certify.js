21ab0f06a2d86b27e36abd3f5c7c663a08d756ae
dmccer <dmccer@gmail.com>
2016-04-15 15:15:05 +0800

    认证提示

--- src/asset/js/const/certify.js
+++ src/asset/js/const/certify.js
@@ -1,2 +1,5 @@
 export const REAL_NAME_CERTIFY_TITLE = '实名认证';
-export const REAL_NAME_CERTIFY_TIP = '为保障交易安全，请先进行实名认证';
+export const REAL_NAME_CERTIFY_TIP_FOR_VIEW = '为保障交易安全，请先进行实名认证';
+export const REAL_NAME_CERTIFY_TIP_FOR_TRUCKER = '为提高发布车源的可信度，请先进行实名认证';
+export const REAL_NAME_CERTIFY_TIP_FOR_PKGER = '为提高发布货源的信任度，请先进行实名认证';
+export const REAL_NAME_CERTIFING_TIP = '实名认证正在审核中,请耐心等待';
