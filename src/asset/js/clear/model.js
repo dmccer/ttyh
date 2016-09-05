a4e4716ef7596f7d470374d57254514c85c8e487
dmccer <dmccer@gmail.com>
2016-09-05 10:47:44 +0800

    解绑

--- src/asset/js/clear/model.js
+++ src/asset/js/clear/model.js
@@ -2,5 +2,5 @@ import querystring from 'querystring';
 import {GET_OPT} from '../const/fetch';
 
 export var UnbindWX = () => {
-  return fetch(`/mvc/wx/logoutAndUnbound`, GET_OPT);
+  return fetch(`/mvc/v3/wx/logoutAndUnbound`, GET_OPT);
 }
